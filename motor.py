f46514f23c164e8608d178ec4c7a508f5cdfb893
Jonathan Stevens <jonathanstevens75@gmail.com>
2016-09-28 16:17:43 +0000

    Minor code cleanup, added stop functionality

--- motor.py
+++ motor.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as io
 io.setmode(io.BCM)
-io.cleanup()
 
 right_1 = 4
 right_2 = 17
@@ -12,6 +11,7 @@ io.setup(right_2, io.OUT)
 io.setup(left_1, io.OUT)
 io.setup(left_2, io.OUT)
 
+# TODO: Make this work
 def set(property, value):
     try:
         f = open("/sys/class/rpi-pwm/pwm0/" + property, 'w')
@@ -20,34 +20,42 @@ def set(property, value):
     except:
         print("Error writing to: " + property + " value: " + value)
  
-set("delayed", "0")
-set("mode", "pwm")
-set("frequency", "500")
-set("active", "1")
+# set("delayed", "0")
+# set("mode", "pwm")
+# set("frequency", "500")
+# set("active", "1")
 
-def clockwise():
+def forward():
     io.output(right_1, True)    
     io.output(right_2, False)
     io.output(left_1, True)
     io.output(left_2, False)
 
-def counter_clockwise():
+def back():
     io.output(right_1, False)
     io.output(right_2, True)
     io.output(left_1, False)
     io.output(left_2, True)
 
-clockwise()
+def stop():
+    io.output(right_1, False)
+    io.output(right_2, False)
+    io.output(left_1, False)
+    io.output(left_2, False)
 
 while True:
-    cmd = raw_input("Command, f/r 0..9, E.g. f5 :")
+    cmd = raw_input("f: forward, b: back >> ")
     direction = cmd[0]
     if direction == "f":
-        clockwise()
-    else: 
-        counter_clockwise()
+        forward()
+    elif direction == "b": 
+        back()
+    else:
+        stop()
     try:
-        #speed = int(cmd[1]) * 11
-        set("duty", "99")
-    except: 
         pass
+        # speed = int(cmd[1]) * 11
+        # set("duty", str(speed))
+    except KeyboardInterrupt: 
+        io.cleanup() 
+
