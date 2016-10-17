302d888e5040cbfb71e1dd28bf9b5c5cc71fcd6c
Jonathan Stevens <jonathanstevens75@gmail.com>
2016-10-17 16:52:11 +0000

    kinda works

--- cat_brain.py
+++ cat_brain.py
@@ -1,19 +1,65 @@
-import motor
-import irsensor
+import RPi.GPIO as io
+import time
 
-def objectDetected():
-    print('here')
-    motor.stop()
+io.setmode(io.BCM)
 
-motor.forward()
-state = False
+right_1 = 4
+right_2 = 17
+left_1 = 27
+left_2 = 22
 
-while True:
-    #state = irsensor.detectChange()
-    if state==0:
-        motor.stop()
-    try:
-        pass
-    except KeyboardInterrupt:
-        motor.stop()
-        io.cleanup()
+sensor = 18
+
+io.setup(right_1, io.OUT)
+io.setup(right_2, io.OUT)
+io.setup(left_1, io.OUT)
+io.setup(left_2, io.OUT)
+
+io.setup(sensor, io.IN, pull_up_down=io.PUD_DOWN)
+
+def forward():
+    print('forward')
+    io.output(right_1, False)
+    io.output(right_2, True)
+    io.output(left_1, False)
+    io.output(left_2, True)
+
+def stop():
+    print('stop')
+    io.output(right_1, False)
+    io.output(right_2, False)
+    io.output(left_1, False)
+    io.output(left_2, False)
+
+# current = io.input(sensor)
+# previous = current
+
+def read_sensor():
+    return io.input(sensor)
+    
+def should_stop():
+    return not(read_sensor())
+    # current = io.input(sensor)
+    # global previous
+    print(read_sensor())
+    #if current != previous:
+    #    previous = current
+    #    return True
+    #return False
+
+forward()
+print('start')
+
+try:
+    while True:
+        time.sleep(1)
+        if should_stop():
+            stop()
+        else:
+            forward()
+
+except KeyboardInterrupt:
+    stop()
+
+finally:
+    io.cleanup()
